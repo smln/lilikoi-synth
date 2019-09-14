18eaf35faec9908334ef2c07379eb22401853dd6
Samuel Cardoza <samlan@pm.me>
2019-09-13 17:00:29 -0700

    added piano, water sounds, meow sound, cymbals, took some stuff out and random fixes

--- src/main.cpp
+++ src/main.cpp
@@ -119,6 +119,7 @@ int main(int argc, char const *argv[])
             canvas.makeColoredImageRectangle(max_hues);
 
             //send three dominant hues to PD, values range 0-180
+
             Synth::pd.sendFloat("huethirdmaxfromcpp", max_hues[2]);
             Synth::pd.sendFloat("huesecondmaxfromcpp", max_hues[1]);
             Synth::pd.sendFloat("huemaxfromcpp", max_hues[0]);
